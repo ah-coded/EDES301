9a43402a1515129b561338f6b95fa65c11584bcc
ah-coded <ah275@rice.edu>
2026-04-24 14:07:40 -0500

    Delete python/button directory

--- python/button/testing.py
+++
@@ -1,9 +0,0 @@
-import Adafruit_BBIO.GPIO as GPIO
-import time
-
-test_pin = "P2_04" # Try P2_04 instead of P2_02
-GPIO.setup(test_pin, GPIO.IN)
-
-print(f"Success! Pin {test_pin} is now an input.")
-print(f"Current Value: {GPIO.input(test_pin)}")
-GPIO.cleanup()
