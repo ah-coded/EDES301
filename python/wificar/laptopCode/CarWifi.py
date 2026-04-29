923898340bed9aa0832793098b468683676975fa
ah-coded <ah275@rice.edu>
2026-04-29 03:58:30 -0500

    Update CarWifi.py

--- python/wificar/laptopCode/CarWifi.py
+++ python/wificar/laptopCode/CarWifi.py
@@ -1,3 +1,34 @@
+# --------------------------------------------------------------------------
+# Wificar - Wifi Acces Script
+# --------------------------------------------------------------------------
+# License:   
+# Copyright 2026 Aiden Hwang
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this 
+# list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, 
+# this list of conditions and the following disclaimer in the documentation 
+# and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors 
+# may be used to endorse or promote products derived from this software without 
+# specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import socket
 import threading
 import time
@@ -11,7 +42,7 @@ from VideoCapture import VideoStream
 #Wifi Access Point IP
 #UDP_IP = "192.168.1.1"
 #hotspot
-UDP_IP = "172.20.10.6"
+UDP_IP = #insert hotspot IP here
 UDP_PORT = 3553
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
